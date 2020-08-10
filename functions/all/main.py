2092da48f2e973f20e333f64ca36350a0c511d47
CSenshi <saba.pochkhua@gmail.com>
2020-08-11 02:32:08 +0400

    implement dummy all users

--- functions/all/main.py
+++ functions/all/main.py
@@ -1,3 +1,30 @@
+import json
+
+
 def users_handler(request):
-    # this function retrieves all users
-    return "Users list"
+    user1 = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "email": "JohnDoe@gmail.com",
+        "age": 30,
+        "city": "New York",
+    }
+    user2 = {
+        "first_name": "Saba",
+        "last_name": "Pochkhua",
+        "email": "Saba.Pochkhua@gmail.com",
+        "age": 22,
+        "city": "Tbilisi",
+    }
+    user3 = {
+        "first_name": "Nika",
+        "last_name": "Losaberidze",
+        "email": "NikaLosa@gmail.com",
+        "age": 22,
+        "city": "Tbilisi",
+    }
+
+    users = [user1, user2, user3]
+
+    # convert into JSON:
+    return json.dumps(users)
