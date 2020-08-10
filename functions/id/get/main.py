b38ee7d1fc76cdb3b1e4d581f87eb304891cb76e
nikalosa <nikalosaberidze@gmail.com>
2020-08-10 21:29:58 +0400

    Change user_handler function and cloudbuild.yaml file

--- functions/id/get/main.py
+++ functions/id/get/main.py
@@ -1,11 +1,10 @@
 def user_handler(request):
+
     request_json = request.get_json(silent=True)
     request_args = request.args
 
-    if request_json and 'userId' in request_json:
-        user_id = str(request_json['userId']) + '/json'
-    elif request_args and 'userId' in request_args:
-        user_id = str(request_args['userId']) + '/args'
+    if request_args and 'userId' in request_args:
+        user_id = request_args['userId']
     else:
         user_id = 'UserId'
 
