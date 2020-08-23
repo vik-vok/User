4be6860c34a94a552fbbeff72333c81d348b9406
CSenshi <saba.pochkhua@gmail.com>
2020-08-23 21:06:41 +0400

    test user get

--- functions/id/get/main.py
+++ functions/id/get/main.py
@@ -8,15 +8,15 @@ client = datastore.Client('speech-similarity')
 def user_get(request):
     request_json = request.get_json(silent=True)
     request_args = request.args
-    if request_json and 'userId' in request_json:
-        user_id = request_json['userId']
-    elif request_args and 'userId' in request_args:
-        user_id = int(request_args['userId'])
-    else:
-        # return error apiresponse
-        return ""
+    return json.dumps([request_args, request_json])
+    # if request_json and 'userId' in request_json:
+    #     user_id = request_json['userId']
+    # elif request_args and 'userId' in request_args:
+    #     user_id = int(request_args['userId'])
+    # else:
+    #     # return error apiresponse
+    #     return ""
 
-    key = client.key('User', user_id)
-    user = client.get(key)
+    # key = client.key('User', user_id)
+    # user = client.get(key)
 
-    return json.dumps(user)
