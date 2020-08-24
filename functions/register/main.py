d056d105dfe1182304f27e54267c22b3f949208a
CSenshi <saba.pochkhua@gmail.com>
2020-08-24 15:58:29 +0400

    text register

--- functions/register/main.py
+++ functions/register/main.py
@@ -13,18 +13,19 @@ def user_register(request):
 
     with datastore_client.transaction():
         # Key = Firestore.uid
+
         complete_key = datastore_client.key('User', request_json['id'])
         user = datastore.Entity(key=complete_key)
 
-        data = {
-            "username": request_json["username"],
-            "email": request_json["email"],
-            "photoUrl": request_json["photoUrl"],
-            "emailVerified": request_json["emailVerified"],
-        }
+    return request_json
+        # data = {
+        #     "username": request_json["username"],
+        #     "email": request_json["email"],
+        #     "photoUrl": request_json["photoUrl"],
+        #     "emailVerified": request_json["emailVerified"],
+        # }
 
-        logger.log_text(data)
-        user.update(data)
-        datastore_client.put(user)
+        # logger.log_text(data)
+        # user.update(data)
+        # datastore_client.put(user)
 
-    return "User successfully registered"
