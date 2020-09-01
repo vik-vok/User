c3d6fccd2024589a31643f6ea2b48675432f825b
nikalosa <nikalosaberidze@gmail.com>
2020-09-02 00:25:39 +0400

    insert userid's into json response

--- functions/all/main.py
+++ functions/all/main.py
@@ -5,5 +5,14 @@ datastore_client = datastore.Client('speech-similarity')
 
 
 def user_get_all(request):
+
     query = datastore_client.query(kind='User')
-    return json.dump(list(query.fetch()))
+    results = list(query.fetch())
+
+    query.keys_only()
+    keys = list(query.fetch())
+
+    for i in range(len(results)):
+        results[i]['id'] = keys[i].id
+
+    return json.dumps(results)
