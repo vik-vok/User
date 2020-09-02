d3f9b459b27acb233cda9a2af134aee7af3cb294
CSenshi <saba.pochkhua@gmail.com>
2020-09-03 00:12:53 +0400

    implement user_original_voices

--- functions/id/originalvoices/get/main.py
+++ functions/id/originalvoices/get/main.py
@@ -1,21 +1,29 @@
 import json
+from google.cloud import datastore
 
+datastore_client = datastore.Client('speech-similarity')
 
-def get_users_original_voices(request):
-    orig_voice1 = {
-        'name': 'levioza',
-        'description': "harry fucking potter",
-        'voiceUrl': 'll.url',
-        'userId': 14
-    }
-    orig_voice2 = {
-        'name': 'misha wadi',
-        'description': "when misha is going",
-        'voiceUrl': 'lll.url',
-        'userId': 14
-    }
+def user_original_voices(request):
+    # 1. Get User ID from request
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+    if request_json and "userId" in request_json:
+        userId = request_json["userId"]
+    elif request_args and "userId" in request_args:
+        userId = request_args["userId"]
+    else:
+        return (json.dumps({"error": "Missing parameter: userId"}), 422, {})
 
-    orig_voices = [orig_voice1, orig_voice2]
+    # 2. Fetch All Original Voices
+    query = datastore_client.query(kind='OriginalVoice')
+    query.add_filter('userId', '=', userId)
+    results = list(query.fetch())
 
-    # convert into JSON:
-    return json.dumps(orig_voices)
+    # 3. Fetch ID's
+    query.keys_only()
+    keys = list(query.fetch())
+    for i in range(len(results)):
+        results[i]['originalVoiceId'] = keys[i].id
+
+    # 4. return JSON data
+    return json.dumps(results)
